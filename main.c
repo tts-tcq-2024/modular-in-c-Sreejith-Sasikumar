c7f753562c0c229a0387718a7d943cd2211fcfbc
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-19 17:32:01 +0530

    Update main.c - testNumberToPair

--- main.c
+++ main.c
@@ -2,30 +2,8 @@
 #include <assert.h> 
 #include "color_codes.h"
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
-}
-
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
-}
-
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
 }
 
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
