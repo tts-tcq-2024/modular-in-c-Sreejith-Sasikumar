ff53103c23bbccf766c056bb08ecc8c2f4d55cc0
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 11:49:42 +0530

    Update color_pair_to_string.c - remove color_codes.h

--- color_pair_to_string.c
+++ color_pair_to_string.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "color_pair_to_string.h"
-#include "color_codes.h"
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
