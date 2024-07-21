0571f563a5f076a2928558fc4e6dcb8dcdf3133c
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 13:10:14 +0530

    Update test_pair_to_number.c

--- test_pair_to_number.c
+++ test_pair_to_number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "test_pair_to_number.h"
+#include "color_codes.h"
 
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
 {
