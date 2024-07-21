5d560da38e0acbe948f9e356a9130ab2d34d505a
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 15:14:17 +0530

    Update color_codes.h - moving inits

--- color_codes.h
+++ color_codes.h
@@ -10,12 +10,6 @@ typedef enum {
     BLUE, ORANGE, GREEN, BROWN, SLATE
 } MinorColor;
 
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 typedef struct {
     MajorColor majorColor;
     MinorColor minorColor;
