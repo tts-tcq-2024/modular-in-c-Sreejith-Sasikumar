26170b6135f3f876848b3a32a35e1baa4cdc7534
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 13:21:12 +0530

    Update color_codes.h

--- color_codes.h
+++ color_codes.h
@@ -1,11 +1,12 @@
 #ifndef COLOR_CODES_H
 #define COLOR_CODES_H
 
-#include <stdio.h>
-#include <assert.h>
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+typedef enum {
+    WHITE, RED, BLACK, YELLOW, VIOLET 
+} MajorColor;
+typedef enum {
+    BLUE, ORANGE, GREEN, BROWN, SLATE
+} MinorColor;
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
