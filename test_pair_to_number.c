297eca89fa59b842586390eb5359d619fd1516eb
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 14:55:29 +0530

    Update test_pair_to_number.c

--- test_pair_to_number.c
+++ test_pair_to_number.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <assert.h>
-#include "get_pair_number_from_color.h"
 #include "test_pair_to_number.h"
 
 void testPairToNumber( MajorColor major, MinorColor minor, int expectedPairNumber)
