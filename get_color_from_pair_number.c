8d2f7a15dd1acb18e554c8fc80f597ab514e7ace
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 13:34:54 +0530

    Update get_color_from_pair_number.c - typedef enum

--- get_color_from_pair_number.c
+++ get_color_from_pair_number.c
@@ -3,7 +3,7 @@
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    colorPair.majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
