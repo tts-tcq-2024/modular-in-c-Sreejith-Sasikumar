e2f827c17400df79b6ff10a401714ad1001f30c2
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 14:40:32 +0530

    Update test_number_to_pair.h

--- test_number_to_pair.h
+++ test_number_to_pair.h
@@ -4,6 +4,6 @@
 #include "get_color_from_pair_number.h"
 #include "color_pair_to_string.h"
 
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor);
 
 #endif
