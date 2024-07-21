62e99f8863881ec8ae4d96eafae8494e9585d094
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 15:15:10 +0530

    Create init.c

---
+++ init.c
@@ -0,0 +1,7 @@
+#include "color_codes.h"
+
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
