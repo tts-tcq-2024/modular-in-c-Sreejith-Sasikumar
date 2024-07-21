450b67472dbd3a2d1e58280f8e0877fb5e129f8b
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 11:56:57 +0530

    Update test_number_to_pair.h - conditional compiling

--- test_number_to_pair.h
+++ test_number_to_pair.h
@@ -1,4 +1,9 @@
+#ifndef TEST_NUMBER_TO_PAIR_H
+#define TEST_NUMBER_TO_PAIR_H
+
 #include "get_color_from_pair_number.h"
 #include "color_pair_to_string.h"
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+
+#endif
