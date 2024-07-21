2265614a7dda8bc876c217d910cb5bf4527947e8
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-07-21 13:15:29 +0530

    Update get_pair_number_from_color.c

--- get_pair_number_from_color.c
+++ get_pair_number_from_color.c
@@ -1,4 +1,5 @@
 #include "get_pair_number_from_color.h"
+#include "color_codes.h"
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
